85a9652180b3ab26e8ec13876b9f00427ce7c2ee
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-11-22 06:56:32 -0300

    Create Square Test

--- reversi/reversi.js
+++ reversi/reversi.js
@@ -10,4 +10,24 @@ function legalMovesForPlayerInTurn(player, squares) {
   return legalMoves;
 }
 
-module.exports = { createReversi, legalMovesForPlayerInTurn };
+function createSquare({ coordinates, busyBy, findFlippingLine }) {
+  function getCoordinates() {
+    return { ...coordinates };
+  }
+  function isLegalMoveFor(player) {
+    const isEmpty = busyBy === null;
+    const flipsAnyOpponentsCounter = findFlippingLine(coordinates, player);
+    const isLegalMove = isEmpty && flipsAnyOpponentsCounter;
+    if (isLegalMove) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return {
+    getCoordinates,
+    isLegalMoveFor,
+  };
+}
+
+module.exports = { createReversi, legalMovesForPlayerInTurn, createSquare };
--- reversi/reversi.test.js
+++ reversi/reversi.test.js
@@ -1,6 +1,56 @@
-const { legalMovesForPlayerInTurn } = require("./reversi");
+const { legalMovesForPlayerInTurn, createSquare } = require("./reversi");
 
 describe("Reversi Test", () => {
+  // getBoardPosition()
+  // getPlayerInTurn()
+  // getSquares()
+
+  describe("Create Square", () => {
+    it("should return {row:2, col:3}", () => {
+      const square = createSquare({ coordinates: { row: 2, col: 3 } });
+      expect(square.getCoordinates()).toEqual({ row: 2, col: 3 });
+    });
+    it("should return false when the square is busy", () => {
+      const square = createSquare({
+        coordinates: { row: 4, col: 2 },
+        busyBy: "B",
+        findFlippingLine: () => {},
+      });
+      expect(square.isLegalMoveFor("W")).toEqual(false);
+    });
+    it("should return true when square is empty and flipping line is found", () => {
+      function findFlippingLine(coordinates, forPlayer) {
+        if (!!coordinates && forPlayer === "W") {
+          return true;
+        }
+      }
+
+      const square = createSquare({
+        coordinates: { row: 0, col: 0 },
+        busyBy: null,
+        findFlippingLine,
+      });
+
+      expect(square.isLegalMoveFor("W")).toEqual(true);
+    });
+    it("should return false when square is empty but no flipping line is found", () => {
+      function findFlippingLine(coordinates, forPlayer) {
+        if (!!coordinates && forPlayer === "B") {
+          return false;
+        }
+        return true;
+      }
+
+      const square = createSquare({
+        coordinates: { row: 3, col: 5 },
+        busyBy: null,
+        findFlippingLine,
+      });
+
+      expect(square.isLegalMoveFor("B")).toEqual(false);
+    });
+  });
+
   describe("Legal Moves for Player in Turn", () => {
     it("should return [] for player W turn", () => {
       const square = {
